977d0569b3cefe5a666864be6f2e1716cbf21601
moomarco <iamwumaixing@gmail.com>
2023-11-13 15:15:10 +0800

    export mystiko core account

--- lib/mystiko_protos_dart.dart
+++ lib/mystiko_protos_dart.dart
@@ -22,5 +22,6 @@ export 'package:mystiko_protos_dart/src/gen/mystiko/core/v1/mystiko.pb.dart';
 export 'package:mystiko_protos_dart/src/gen/mystiko/core/handler/v1/account.pb.dart';
 export 'package:mystiko_protos_dart/src/gen/mystiko/core/handler/v1/wallet.pb.dart';
 export 'package:mystiko_protos_dart/src/gen/mystiko/core/document/v1/wallet.pb.dart';
+export 'package:mystiko_protos_dart/src/gen/mystiko/core/document/v1/account.pb.dart';
 export 'package:mystiko_protos_dart/src/gen/mystiko/service/v1/client.pb.dart';
 export 'package:mystiko_protos_dart/src/gen/mystiko/storage/v1/filter.pb.dart';
