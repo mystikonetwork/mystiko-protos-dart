8c316ac2bcf854d5cc456a5df3bbefda642110cc
X <96100679+moomarco@users.noreply.github.com>
2023-12-21 19:17:17 +0800

    export scanner and synchronizer api (#13)

---
+++ lib/mystiko/api/scanner/index.dart
@@ -0,0 +1,4 @@
+library api_scanner;
+
+export 'package:mystiko_protos_dart/src/gen/mystiko/api/scanner/v1/request.pb.dart';
+export 'package:mystiko_protos_dart/src/gen/mystiko/api/scanner/v1/response.pb.dart';
---
+++ lib/mystiko/api/synchronizer/index.dart
@@ -0,0 +1,4 @@
+library api_synchronizer;
+
+export 'package:mystiko_protos_dart/src/gen/mystiko/api/synchronizer/v1/request.pb.dart';
+export 'package:mystiko_protos_dart/src/gen/mystiko/api/synchronizer/v1/response.pb.dart';
