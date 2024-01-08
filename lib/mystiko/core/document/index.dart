ab2f120062c1372a9a68d6b27bf70adffb88044f
moomarco <iamwumaixing@gmail.com>
2024-01-08 16:23:33 +0800

    update

--- lib/mystiko/core/document/index.dart
+++ lib/mystiko/core/document/index.dart
@@ -3,3 +3,4 @@ library document;
 export 'package:mystiko_protos_dart/src/gen/mystiko/core/document/v1/wallet.pb.dart';
 export 'package:mystiko_protos_dart/src/gen/mystiko/core/document/v1/account.pb.dart';
 export 'package:mystiko_protos_dart/src/gen/mystiko/core/document/v1/deposit.pb.dart';
+export 'package:mystiko_protos_dart/src/gen/mystiko/core/document/v1/spend.pb.dart';
