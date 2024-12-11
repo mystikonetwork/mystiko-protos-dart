f9eeec426f5037d66e8bf915f8ee3981cb86057f
X <96100679+moomarco@users.noreply.github.com>
2024-12-11 15:00:06 +0800

    fix bugs and add workflows (#23)
    * fix bugs and add workflows
    
    * fix bugs

--- gen.sh
+++ gen.sh
@@ -6,6 +6,7 @@ buf generate \
   --path schema/proto/mystiko/core/synchronizer/v1 \
   --path schema/proto/mystiko/common/v1 \
   --path schema/proto/mystiko/core/handler/v1 \
+  --path schema/proto/mystiko/screening/v1 \
   --path schema/proto/mystiko/storage/v1 \
   --path schema/proto/mystiko/core/document/v1 \
   --path schema/proto/mystiko/loader/v1 \
