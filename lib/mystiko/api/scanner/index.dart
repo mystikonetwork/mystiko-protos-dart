703919b0ae03d03e126c7ec9df407b345fb9aa87
moomarco <iamwumaixing@gmail.com>
2024-12-19 17:38:20 +0800

    update

--- lib/mystiko/api/scanner/index.dart
+++ lib/mystiko/api/scanner/index.dart
@@ -1,2 +1,6 @@
-export 'package:mystiko_protos_dart/src/gen/mystiko/api/scanner/v1/request.pb.dart';
+export 'package:mystiko_protos_dart/src/gen/mystiko/api/scanner/v1/request.pb.dart' hide SyncRequest;
 export 'package:mystiko_protos_dart/src/gen/mystiko/api/scanner/v1/response.pb.dart';
+
+import 'package:mystiko_protos_dart/src/gen/mystiko/api/scanner/v1/request.pb.dart';
+
+var scannerSyncRequest = SyncRequest;
